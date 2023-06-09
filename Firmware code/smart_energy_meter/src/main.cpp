d8b1bb9390a3fb309118bd573825008d626a2856
Adebo Adeola <adebo.adeola@gmail.com>
2023-06-09 09:16:12 +0100

    edit enviroment for debug

--- Firmware code/smart_energy_meter/src/main.cpp
+++ Firmware code/smart_energy_meter/src/main.cpp
@@ -22,6 +22,7 @@
 #include "time_management.hpp"
 #include "token_management.hpp"
 
+// meter_const_config deviceConfig;
 HardwareSerial Serial2(PA3, PA2);
 
 byte fe1[8] = {0b00011, 0b00011, 0b00011, 0b00011,
@@ -44,8 +45,11 @@ void setup() {
   pinMode(red_led, OUTPUT);
   pinMode(green_led, OUTPUT);
 
+  Serial2.begin(115200);
+  Serial2.print("Device Powered! \n");
   digitalWrite(buzzer, HIGH);
 
+  Serial2.print("Setting up LCD... \n");
   lcd.createChar(0, fe1);
   lcd.createChar(1, fe2);
   lcd.createChar(2, fe3);
@@ -54,7 +58,8 @@ void setup() {
   lcd.print("  DEVELOPED BY  ");
   lcd.setCursor(0, 1);
   lcd.print(" FIRST ELECTRIC ");
-
+  delay(2000);
+  
   lcd.clear();
   lcd.setCursor(7, 0);
   lcd.write(byte(0));
@@ -64,14 +69,15 @@ void setup() {
   lcd.write(byte(2));
   lcd.setCursor(8, 1);
   lcd.write(byte(3));
-
+  Serial2.print("LCD Setup complete! \n");
+   
+  Serial2.print("Setting up AFE... \n");
   ATM90E26.begin(9600);
   AFE_chip.SET_register_values();
   delay(1000);
+  Serial2.print("AFE Setup Complete! \n");
   digitalWrite(buzzer, LOW);
 
-  Serial2.begin(115200);
-  Serial2.print("working");
   Serial1.begin(115200);
   delay(3000);
   Serial1.write("AT+IPR=9600\r\n");
@@ -81,7 +87,7 @@ void setup() {
   lcd.setCursor(0, 0);
   lcd.print(" SYSTEM BOOTING ");
   lcd.setCursor(0, 1);
-  lcd.print(" ##             ");
+  lcd.print(" #------------#");
 
   Serial2.println("Initializing modem...");
   modem.restart();
@@ -100,12 +106,11 @@ void setup() {
   delay(2000);
   lcd.clear();
 
-  if (!rtc.begin()) {
-    lcd.print("Couldn't find RTC");
+  while(rtc.begin() == false ) { 
+    lcd.print("Couldn't find RTC"); 
     delay(2000);
-    while (1)
-      ;
-  }
+  } 
+
   if (!rtc.isrunning()) {
     lcd.print("RTC is NOT running!");
     delay(2000);
@@ -132,6 +137,15 @@ void setup() {
   if (creditt >= 0) {
     mem.writeLong(credit_eeprom_location, creditt);
   }
+
+  /* Get device configuration parameters from eeprom */
+  // deviceConfig =  mem.readLong(deviceConfig_location);
+  // while(deviceConfig.status == NOT_SET_CONFIG)
+  // {
+  //   Serial2.println("Device configuration required...\n");
+  //   while()  //wait for command from serial 2
+  // }
+
   delay(10);
   relay_on();
   select_mode();
@@ -148,6 +162,7 @@ void setup() {
   } else {
     printf("OpenPAYGO code written here");
   }
+Serial2.println("Setup Complete! \n");  
 }
 
 void loop() {
